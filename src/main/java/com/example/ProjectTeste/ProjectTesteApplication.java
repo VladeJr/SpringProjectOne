f4c88d4ad2d3fcbe2a6ed1ccd00fe76204ca2fbe
Nekaytsu <nekaytsu@gmail.com>
2023-11-19 16:22:13 -0300

    Project One

---
+++ src/main/java/com/example/ProjectTeste/ProjectTesteApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectTeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjectTeste/ProjectTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectTeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
